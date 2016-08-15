b3a22cf2801b4cbce2b0794f854dbe6906617976
zhanglun <zhanglun1410@gmail.com>
2016-08-15 23:19:45 +0800

    add new py

--- spider/entry.py
+++ spider/entry.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-root = 'https://api.douban.com/v2/movie/search?tag=%E5%96%9C%E5%89%A7'
+root = 'http://api.douban.com//v2/movie/top250'
 
 
 def fetchJSON ():
@@ -7,7 +7,7 @@ def fetchJSON ():
   data = response.read()  # type: bytes
   string = str(data, encoding='utf-8') # type: string
   dataDict = json.loads(string) # dict
-  print(type(dataDict))
+  print(dataDict)
 
 
 def start():
