1fb85f09865afdec1c0dc157d8377647f249572e
zhanglun <zhanglun1410@gmail.com>
2016-08-22 18:12:30 +0800

    update

--- app/server.py
+++ app/server.py
@@ -1,11 +1,20 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
+def index():
+    return 'Index Page'
 
-app.debug = True;
 
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
+
+app.debug = True
+
+# 确保服务器只会在该脚本被 Python 解释器直接执行的时候才会运行，而不是作为模块导入的时候
 if __name__ == '__main__':
-  app.run()
+    app.run()
