e0f2d8dbc942fc0b110efe2240141d8f47a682c2
zhanglun <zhanglun1410@gmail.com>
2016-08-29 08:33:15 +0800

    api return json

--- app/views/api.py
+++ app/views/api.py
@@ -1,13 +1,19 @@
-from flask import request, current_app
+from flask import jsonify
 from flask import Blueprint
 from app.models import mongo
+from bson import json_util
+import json
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
 @api.route('/', methods=['GET'])
 def index():
-    users = mongo.db.users.find({})
-    for user in users:
-        print(user)
-    return "{'page': 'API'}"
+    movies = mongo.db.movies.find({})
+    result = [];
+    for movie in movies:
+        result.append(movie)
+
+    print(type(json.loads(json_util.dumps(result))))
+    print(type(json_util.dumps(result)))
+    return jsonify(json.loads(json_util.dumps(result)))
