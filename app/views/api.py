cd8d81b9fa7b30168daf8eee67bc54540775d1c1
zhanglun <zhanglun1410@gmail.com>
2016-08-29 17:07:26 +0800

    user Response return data

--- app/views/api.py
+++ app/views/api.py
@@ -1,13 +1,25 @@
-from flask import request, current_app
+from flask import request, current_app, Response
 from flask import Blueprint
 from app.models import mongo
+import json
+from bson.objectid import ObjectId
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api.route('/', methods=['GET'])
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        else:
+            return obj
+
+
+@api.route('/movies', methods=['GET'])
 def index():
-    users = mongo.db.users.find({})
-    for user in users:
-        print(user)
-    return "{'page': 'API'}"
+    movies = mongo.db.movies.find({})
+    data = list()
+    for movie in movies:
+        data.append(movie)
+
+    return Response(json.dumps(data, cls=Encoder), mimetype='application/json')
