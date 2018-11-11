334f4e272ea75209f96009d1df2a22d5a6ade08b
zhanglun <zhanglun1410@gmail.com>
2018-11-11 22:31:19 +0800

    update sql

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+nohup pyspider -c config.json&
 nohup pyspider -c config.json fetcher > fetcher.1.out&
 nohup pyspider -c config.json fetcher > fetcher.1.out&
 nohup pyspider -c config.json fetcher > fetcher.1.out&
